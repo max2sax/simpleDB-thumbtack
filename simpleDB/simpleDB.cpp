8dacdb18b8409a7112cb234662744c0bb41b3503
Michael Archambault <marchambault@jpg.com>
2016-08-06 14:52:28 -0400

    create basic structure for database. add key and value class. add database class. finish main run loop.

--- simpleDB/simpleDB.cpp
+++ simpleDB/simpleDB.cpp
@@ -26,13 +26,82 @@
 #include <unordered_map>
 
 using namespace std;
+class Key;
+
+class Value {
+public: 
+	Value(){ m_value = 0; };
+	Value(Value& other){ 
+		m_value = other.m_value; 
+		for (auto&& key : other.references) {
+			references.push_back(key); 
+		}
+	};
+private: 
+	int m_value; 
+	std::vector<Key&> references; 
+};
+class Key {
+private: 
+	std::string m_key; 
+	Value* m_value; 
+public: 
+	Key(){ m_key = "", m_value = 0; };
+	Key(std::string keyName) : m_key(keyName) { };
+	Key(std::string keyName, Value* v) : m_key(keyName), m_value(v) {};
+	Key(Key& other){ m_key = other.m_key; m_value = new Value(*(other.m_value)); };
+};
+class DataBase {
+private: 
+	std::vector<Key&> keys; 
+	Value* values; 
+public: 
+	DataBase(){ values = nullptr_t; };
+	DataBase(DataBase& original) {};
+	~DataBase(){}; 
+};
+
 class Command {
 private:
 	std::string command; 
+	std::string variable; 
+	int value; 
 public:
-	Command(); 
-	Command(std::string com); 
-	~Command(); 
+	Command() { command = ""; variable = ""; value = 0; };
+	Command(std::string &com) : command(com){};
+	~Command() {}; //nothing to delete};
+	friend std::stringstream& operator>>(stringstream& is, Command& c) {
+		//read in command info, and set invalid if cannot parse. 
+		try {
+			is >> c.command;
+			if (!is.eof()) {
+				//read next, which will be a variable name: 
+				is >> c.variable; 
+			}
+			if (!is.eof()) {
+				//read next, which will be a variable name: 
+				is >> c.value;
+			}
+		}
+		catch (exception e){
+			c.command = ""; 
+			c.variable = ""; 
+			c.value = 0; 
+		}
+		return is; 
+	};
+	friend std::ostream& operator<<(ostream& out, Command& c) {
+		//read in command info, and set invalid if cannot parse. 
+		try {
+			out << c.command << " ";
+			out << c.variable << " ";
+			out << c.value << " ";
+		}
+		catch (exception e){
+			out << "invalid command"; 
+		}
+		return out; 
+	};
 }; 
 
 class Transaction{
@@ -49,10 +118,19 @@ public:
 
 int _tmain() {
 	/* Enter your code here. Read input from STDIN. Print output to STDOUT */
-	std::string input; 
+	std::string input = ""; 
 	std::deque<Transaction> transactionList;
-	while ((input << cin.getline()) != "END") {
 
+
+	while (input.compare("END") != 0) {
+		std::getline(cin, input); //get new input line
+		cout << input << std::endl; //echo the input
+		//parse the input stream for the command:
+		std::stringstream instream(input); 
+		Command c; 
+		instream >> c;
+		c.execute(); 
+		//alkdjfadlkjasdsdfsdfsdfdfsfff
 	}
 	return 0;
 }
